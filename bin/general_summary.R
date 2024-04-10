8d2a63c0311aaad8d685a6e953f9ba21a6dfa5b6
David Schaeper <dwschaeper12@gmail.com>
2024-04-10 12:51:29 -0400

    Updating plots

--- bin/general_summary.R
+++ bin/general_summary.R
@@ -1,21 +1,58 @@
 library(ggplot2)
+library(ggpubr)
 args <- commandArgs(T)
 theme_set(theme_bw())
 
-input = args[1]
+overall_pie <- function(frame) {
+  # aggregate totals
+  frame <- aggregate(frame$Abundance, by=list(frame$Lineage), FUN=sum)
+  frame$ratio <- round(frame$x / sum(frame$x), 4) * 100
+  frame$Group.1 <- ifelse(frame$ratio > 5, frame$Group.1, 'Other')
+  frame <- aggregate(frame$ratio, by=list(frame$Group.1), FUN=sum)
+  
+  # generate plot
+  ggplot(frame, aes(x='', y=x, fill=Group.1)) +
+    geom_bar(stat='identity', width=1) +
+    coord_polar('y', start=0) +
+    theme_void() +
+    geom_text(aes(label=paste0(x, '%')), position=position_stack(vjust=0.5)) +
+    scale_fill_discrete(name='Lineage') +
+    ggtitle('Overall Demix Result')
+}
 
+individual <- function(sample, frame) {
+  frame <- subset(frame, frame$Sample == sample)
+  frame$ratio <- round(frame$Abundance / sum(frame$Abundance), 4) * 100
+  frame$Lineage <- ifelse(frame$ratio > 5, frame$Lineage, 'Other')
+  frame <- aggregate(frame$ratio, by=list(frame$Lineage), FUN=sum)
+  
+  ggplot(frame, aes(x='', y=x, fill=Group.1)) +
+    geom_bar(stat='identity', width=1) +
+    coord_polar('y', start=0) +
+    theme_void() +
+    geom_text(aes(label=paste0(x, '%')), position=position_stack(vjust=0.5)) +
+    scale_fill_discrete(name='Lineage') +
+    ggtitle(sample)
+}
+
+# read the info
+input <- args[1]
 results <- read.csv(input, sep=',')
-totals <- aggregate(results$Abundance, by=list(results$Lineage), FUN=sum)
-totals$ratio <- round(totals$x / sum(totals$x), 2)
-totals$Group.1 <- ifelse(totals$x > 0.5, totals$Group.1, 'Other')
-totals <- aggregate(totals$ratio, by=list(totals$Group.1), FUN=sum)
+
+# overall plot
+overall <- overall_pie(results)
+
+# order the frame so individual plots are easy to find
+results <- results[order(results$Sample),]
+
+# individual plots
+samples <- unique(results$Sample)
+individaual_plots <- lapply(samples, individual, results)
+
+rows <- round((length(individaual_plots) + 1) / 2, 0)
+plot <- ggarrange(overall, plotlist=individaual_plots, ncol=2, nrow=rows)
+
+title <- paste0('demix_summary_', Sys.Date(), '.pdf')
+ggsave(title, plot)
 
 
-ggplot(totals, aes(x='', y=x, fill=Group.1)) +
-  geom_bar(stat='identity', width=1) +
-  coord_polar('y', start=0) +
-  theme_void() +
-  geom_text(aes(label=paste0(x, '%')), position=position_stack(vjust=0.5)) +
-  scale_fill_discrete(name='Lineage')
-title <- paste0('overall_lineage_presence', Sys.Date(), '.png')
-ggsave(title, bg='white')
